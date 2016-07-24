b9a59b9aa91159bb7f64233a11a4a90548d728a1
ganya <ganya555@gmail.com>
2016-07-24 15:28:24 +0300

    убрал jquery ui

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,6 +2,5 @@
 
 module.exports = [
   './bower_components/jquery/dist/jquery.min.js',
-  './bower_components/jquery-validation/dist/jquery.validate.min.js',
-  './bower_components/jquery-ui/jquery-ui.min.js'
+  './bower_components/jquery-validation/dist/jquery.validate.min.js'
 ];
