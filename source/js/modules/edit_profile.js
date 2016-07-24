296849a943bd8d540625e2978ed55333720d7d39
ganya <ganya555@gmail.com>
2016-07-24 15:27:01 +0300

    добавил анимацию edit profile карточки и повесил событие на кнопку редактировать

--- source/js/modules/edit_profile.js
+++ source/js/modules/edit_profile.js
@@ -1,8 +1,34 @@
 'use strict';
 
-$('.edit-block__btn').on('click',function (e) {
-    e.preventDefault();
-    $('.edit-profile-wrap').css({
-        'display':'block'
-    })
+var profile = $('.edit-profile-wrap'),
+    profileCard = $('.edit-profile'),
+    editBtn = $('.edit-block__btn'),
+    closeBtn = $('.cancel-popup__btn'),
+    indexClick = 0;
+
+$ ( function() {
+    editBtn.on('click',( function(e) {
+        if (indexClick === 0) {
+            profile.fadeIn(100);
+            profileCard.addClass('profileSlideDown');
+            indexClick = 1;
+        }
+        else {
+            profile.fadeOut(100);
+            profileCard.removeClass('profileSlideDown');
+            indexClick = 0;
+        }
+        e.stopPropagation();
+    }));
+    closeBtn.on('click',function () {
+        profile.fadeOut(100);
+        profileCard.removeClass('profileSlideDown');
+    });
 });
+$(document).on('click', (function(e) {
+    if ($(e.target).closest(".edit-profile").length) return;
+    profile.fadeOut(100);
+    profileCard.removeClass('profileSlideDown');
+    indexClick = 0;
+    e.stopPropagation();
+}));
