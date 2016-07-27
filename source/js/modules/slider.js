32f77591233ad00b11773f61129cbe214a91e60b
dmitrygavrish <dmitrygavrish@gmail.com>
2016-07-27 23:07:50 +0300

    small fixes; slider item popup improved

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,14 +1,26 @@
 $(function() {
-  $('.tile-top').on('click', function() {
-    $('.bg-mask').css('z-index','4').animate({
-      opacity: '1'
-    }, 300);
-    $('.slider-item').css('z-index','5').animate({
-      opacity: '1'
-    }, 300);
+
+  $(document).on('click', function (event) {
+    var target = $(event.target),
+        offset = $(window).scrollTop() + 25;
+
+    if (target.closest('.a-tile-top').length === 1 ||
+        target.closest('.tile-top').length === 1 || target.is('.a-tile-menu__a')) {
+
+      event.preventDefault();
+
+      $('.bg-mask').css('z-index','4').animate({
+        opacity: '1'
+      }, 300);
+      $('.slider-item').css('z-index','5').animate({
+        top: offset + 'px',
+        opacity: '1'
+      }, 300);
+    }
   });
 
   $('.bg-mask').on('click', function() {
+
     $(this).animate({
       opacity: '0'
     }, 200, function() {
@@ -17,7 +29,10 @@ $(function() {
     $('.slider-item').animate({
       opacity: '0'
     }, 200, function() {
-      $(this).css('z-index','-1');
+      $(this).css({
+        'z-index': '-1',
+        'top': '-100%'
+      });
     });
   });
 });
