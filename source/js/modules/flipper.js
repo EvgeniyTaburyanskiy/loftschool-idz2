0b13e8c95dc4d1634e1b40b10c63ba403fba28bb
dmitrygavrish <dmitrygavrish@gmail.com>
2016-07-29 17:39:46 +0300

    flipper fixed

--- source/js/modules/flipper.js
+++ source/js/modules/flipper.js
@@ -1,20 +1,30 @@
 jQuery(document).ready(function(){
+  var switcher = false;
 
-        jQuery('#reg').on('click', function(event) {
-             jQuery('.flip-cnt-w').addClass('hover')
-             event.stopPropagation();
-        });
-       
-        jQuery(document).on('click', function(event) {        
-             if ($(event.target).closest(".hover").length) return;
-             	 jQuery('.flip-cnt-w').removeClass('hover');	
-        });
+  jQuery('#reg').on('click', function(event) {
+    jQuery('.flip-cnt-w').addClass('hover');
+    event.stopPropagation();
+  });
 
-        jQuery('#pass').on('click', function(event) {
-             if ($(event.target).closest(".hidden").length) return;
-                 jQuery('.pass-rec').removeClass('hidden');
-             jQuery('.flip-cnt').addClass('hidden')
-             event.stopPropagation();
-        });
-       
-    });
+  jQuery(document).on('click', function(event) {
+    var target = $(event.target);
+
+    if ( target.closest('.card').length === 0 || target.is('.card-a') ) {
+      jQuery('.flip-cnt-w').removeClass('hover');
+    }
+
+    if ( (target.closest('.card').length === 0 || target.is('.card-a')) && switcher === true) {
+      switcher = false;
+      jQuery('.pass-rec').addClass('hidden');
+      jQuery('.flip-cnt').removeClass('hidden');
+      event.stopPropagation();
+    }
+  });
+
+  jQuery('#pass').on('click', function(event) {
+    switcher = true;
+    jQuery('.pass-rec').removeClass('hidden');
+    jQuery('.flip-cnt').addClass('hidden');
+    event.stopPropagation();
+  });
+});
