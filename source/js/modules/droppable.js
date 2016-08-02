60130929fbda86990021ca5f110e5a133eae05b0
ganya <ganya555@gmail.com>
2016-08-02 17:06:40 +0300

    no message

---
+++ source/js/modules/droppable.js
@@ -0,0 +1,5 @@
+'use strict';
+
+($(function () {
+    $('.dz-remove').empty().html('<svg class="erase-photo__svg" role="img"><use xlink:href="/assets/img/icons/sprite.svg#close"></use></svg>')
+}));
